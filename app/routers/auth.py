27b00f36ccef501724d9b894d605bec2825268d1
Dandefensor <ad79702@gmail.com>
2025-09-29 09:07:15 -0400

    Se agregaron endpoints de GET y POST

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -7,6 +7,19 @@ from db import SessionDep
 router = APIRouter()
 
 
-@router.get("/user")
-async def user():
-    return {"messege" : "Prueba"}
+
+@router.post("/user", response_model = User, tags = ["Users"])
+async def userCreate(user_data : UserCreate, session : SessionDep):
+    user = User.model_validate(user_data.model_dump())
+    session.add(user)
+    session.commit()
+    session.refresh(user)
+    return user
+
+
+@router.get("/user", response_model = list[User], tags = ["Users"])
+async def userList(session : SessionDep):
+    statament = select(User)
+    result = session.exec(statament)
+    user = result.all()
+    return user
